09a6ea43428eabb96f0856fe8668e9dd37958f0c
Mervyn Vangenechten <mervynvangenechten@gmail.com>
2025-09-07 22:29:31 +0200

    --

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,22 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    
+    let offset;
+    if (window.innerWidth <= 768) {
+      offset = window.innerHeight * 0.125;
+    } else {
+      offset = window.innerHeight * 0.15;
+    }
+
+    window.scrollTo({
+      top: target.offsetTop - offset,
+      behavior: 'smooth'
+    });
+  });
+});
